56776ddbf5c9f2e69b21fb6047183ad6ebe7012d
Erica Norby <ericanorby@gmail.com>
2017-02-14 15:37:38 -0500

    finished MVP

--- app.rb
+++ app.rb
@@ -15,6 +15,17 @@ get "/pokemon/new" do
   erb(:"pokemon/new")
 end
 
+get "/pokemon/:id/edit" do
+  @pokemon = Pokemon.find(params[:id])
+  erb(:"pokemon/edit")
+end
+
+put "/pokemon/:id" do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.update(params[:pokemon])
+  redirect("/pokemon/#{@pokemon.id}")
+end
+
 post "/pokemon" do
   params[:pokemon][:img_url] = "http://img.pokemondb.net/artwork/#{params[:pokemon][:name].downcase}.jpg"
   params[:pokemon][:cp] = rand(800)
@@ -25,4 +36,10 @@ end
 get "/pokemon/:id" do
   @pokemon = Pokemon.find(params[:id])
   erb(:"pokemon/show")
+end
+
+delete "/pokemon/:id" do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.destroy
+  redirect("/pokemon")
 end
